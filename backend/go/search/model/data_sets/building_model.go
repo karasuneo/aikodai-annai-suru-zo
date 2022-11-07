613b40c3f922f57333ac0ac1b1306d8fc08332d1
Yusei Mizutani <yusei4520030401@gmail.com>
2022-11-07 11:25:03 +0900

    Dockerの追加

---
+++ backend/go/search/model/data_sets/building_model.go
@@ -0,0 +1,48 @@
+package model_data_sets
+
+// Building テーブル情報
+type Building struct {
+	BuildingName string      `gorm:"primarykey:BuildingName"`
+	ClassRooms   []ClassRoom `gorm:"foreignkey:BuildingName"`
+	Latitude     string
+	Longitude    string
+}
+
+//Buildingの全てのデータを取得
+func GetBuildingAll() []*Building {
+	result := CombineBuildingWithClassRoom()
+	return result
+}
+
+//建物名を検索してデータを取得
+func GetBuildingBuildingName(building_name string) []*Building {
+	result := CombineBuildingWithClassRoom()
+	for _, r := range result {
+		if r.BuildingName != building_name {
+			r = result[len(result)-1]
+		}
+	}
+	return result
+}
+
+//経度を検索してデータを取得
+func GetLatitude(latitude string) []*Building {
+	result := CombineBuildingWithClassRoom()
+	for _, r := range result {
+		if r.BuildingName != latitude {
+			r = result[len(result)-1]
+		}
+	}
+	return result
+}
+
+//緯度を検索してデータを取得
+func GetLongitude(longitude string) []*Building {
+	result := CombineBuildingWithClassRoom()
+	for _, r := range result {
+		if r.BuildingName != longitude {
+			r = result[len(result)-1]
+		}
+	}
+	return result
+}
---
+++ backend/go/search/model/data_sets/combine_table.go
@@ -0,0 +1,43 @@
+package model_data_sets
+
+//BuildingテーブルとClassRoomテーブルを結合
+func CombineBuildingWithClassRoom() []*Building {
+	//構造体の定義
+	building := []*Building{}
+	class_room := CombineClassRoomWithSubject()
+
+	//DBのデータを構造体の配列に格納
+	db.Find(&building)
+
+	//BuilgingのClassRoomに構造体を入れる
+	for _, b := range building {
+		for _, c := range class_room {
+			if b.BuildingName == c.BuildingName {
+				b.ClassRooms = append(b.ClassRooms, *c)
+			}
+		}
+	}
+	return building
+}
+
+//ClassRoomテーブルとSubjectテーブルを結合
+func CombineClassRoomWithSubject() []*ClassRoom {
+	//構造体の定義
+	class_room := []*ClassRoom{}
+	subject := []*Subject{}
+
+	//DBのデータを構造体の配列に格納
+	db.Find(&class_room)
+	db.Find(&subject)
+
+	//ClassRoomのSubjectに構造体を入れる
+	for _, c := range class_room {
+		for _, s := range subject {
+			if c.RoomNumber == s.ClassRoom {
+				c.Subjects = append(c.Subjects, *s)
+			}
+		}
+	}
+
+	return class_room
+}
