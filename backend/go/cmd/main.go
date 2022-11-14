e5bddccd1849be10ebe927de3752784437e9bdba
Yusei Mizutani <yusei4520030401@gmail.com>
2022-11-14 15:32:37 +0900

    APIサーバの作成

--- backend/go/cmd/main.go
+++ backend/go/cmd/main.go
@@ -1,32 +1,34 @@
-// package main
-
-// import (
-// 	"github.com/gin-gonic/gin"
-// 	DataSets "go/search/model/data_sets"
-// )
-
-// func main() {
-// 	r := gin.Default()
-// 	r.GET("/", func(c *gin.Context) {
-// 		d := DataSets.GetBuildingBuildingName("1号")
-// 		c.JSON(200, gin.H{
-// 			"message": "pong",
-// 			"message": d[2],
-// 		})
-// 	})
-// 	r.Run()
-// }
-
 package main
 
 import (
-	DataSets "go/search/model/data_sets"
-	//Student "go/search/model/student"
 	"fmt"
+	"github.com/gin-gonic/gin"
+	DataSets "go/search/model/data_sets"
 )
 
-//動作確認
 func main() {
 	d := DataSets.GetBuildingBuildingName("1号")
 	fmt.Println(d[2])
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		
+		c.JSON(200, gin.H{
+			"result": d[2],
+		})
+	})
+	r.Run()
 }
+
+// package main
+
+// import (
+// 	DataSets "go/search/model/data_sets"
+// 	//Student "go/search/model/student"
+// 	"fmt"
+// )
+
+// //動作確認
+// func main() {
+// 	d := DataSets.GetBuildingBuildingName("1号")
+// 	fmt.Println(d[2])
+// }
