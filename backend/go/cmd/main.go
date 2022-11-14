bca7aac25721ff061232e122a2094447d44a27fc
Yusei Mizutani <yusei4520030401@gmail.com>
2022-11-14 23:47:45 +0900

    ginでApiサーバの作成

--- backend/go/cmd/main.go
+++ backend/go/cmd/main.go
@@ -1,19 +1,19 @@
 package main
 
 import (
-	"fmt"
+	
 	"github.com/gin-gonic/gin"
 	DataSets "go/search/model/data_sets"
 )
 
 func main() {
-	d := DataSets.GetBuildingBuildingName("1号")
-	fmt.Println(d[2])
+
+	
 	r := gin.Default()
 	r.GET("/", func(c *gin.Context) {
-		
+		d := DataSets.GetBuildingAll()
 		c.JSON(200, gin.H{
-			"result": d[2],
+			"result": d,
 		})
 	})
 	r.Run()
