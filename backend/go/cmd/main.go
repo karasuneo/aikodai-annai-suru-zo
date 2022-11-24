1aab02454b8f6f17e14f451b17428c884c1e69de
Yusei Mizutani <yusei4520030401@gmail.com>
2022-11-24 10:04:30 +0900

    ソースコードの分割

--- backend/go/cmd/main.go
+++ backend/go/cmd/main.go
@@ -1,42 +1,9 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
-	"github.com/karasuneo/aikodai-annai-suru-zo/go/model"
+	"github.com/karasuneo/aikodai-annai-suru-zo/go/router"
 )
 
 func main() {
-
-	r := gin.Default()
-	r.GET("/allData", func(c *gin.Context) {
-		result := model.GetBuildingAll()
-		c.JSON(200, gin.H{
-			"data": result,
-		})
-	})
-	r.GET("/buildingName", func(c *gin.Context) {
-		result := model.GetSubjectName("組み込みプログラミング及び演習")
-		c.JSON(200, gin.H{
-			"data": result,
-		})
-	})
-	r.GET("/roomName", func(c *gin.Context) {
-		result := model.GetSubjectName("組み込みプログラミング及び演習")
-		c.JSON(200, gin.H{
-			"data": result,
-		})
-	})
-	r.GET("/subjectName", func(c *gin.Context) {
-		result := model.GetSubjectName("組み込みプログラミング及び演習")
-		c.JSON(200, gin.H{
-			"data": result,
-		})
-	})
-	r.GET("/grade", func(c *gin.Context) {
-		result := model.GetSubjectName("組み込みプログラミング及び演習")
-		c.JSON(200, gin.H{
-			"data": result,
-		})
-	})
-	r.Run()
+	router.Init()
 }
