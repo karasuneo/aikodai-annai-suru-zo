2532e0baef213b0bf0bd672f25f6217b1c7fcbbc
Yusei Mizutani <yusei4520030401@gmail.com>
2022-11-24 09:07:32 +0900

    変数名の変更

--- backend/go/cmd/main.go
+++ backend/go/cmd/main.go
@@ -8,28 +8,34 @@ import (
 func main() {
 
 	r := gin.Default()
-	r.GET("/buildingName", func(c *gin.Context) {
-		d := model.GetSubjectName("組み込みプログラミング及び演習")
+	r.GET("/allData", func(c *gin.Context) {
+		result := model.GetBuildingAll()
 		c.JSON(200, gin.H{
-			"result": d,
+			"data": result,
+		})
+	})
+	r.GET("/buildingName", func(c *gin.Context) {
+		result := model.GetSubjectName("組み込みプログラミング及び演習")
+		c.JSON(200, gin.H{
+			"data": result,
 		})
 	})
 	r.GET("/roomName", func(c *gin.Context) {
-		d := model.GetSubjectName("組み込みプログラミング及び演習")
+		result := model.GetSubjectName("組み込みプログラミング及び演習")
 		c.JSON(200, gin.H{
-			"result": d,
+			"data": result,
 		})
 	})
 	r.GET("/subjectName", func(c *gin.Context) {
-		d := model.GetSubjectName("組み込みプログラミング及び演習")
+		result := model.GetSubjectName("組み込みプログラミング及び演習")
 		c.JSON(200, gin.H{
-			"result": d,
+			"data": result,
 		})
 	})
 	r.GET("/grade", func(c *gin.Context) {
-		d := model.GetSubjectName("組み込みプログラミング及び演習")
+		result := model.GetSubjectName("組み込みプログラミング及び演習")
 		c.JSON(200, gin.H{
-			"result": d,
+			"data": result,
 		})
 	})
 	r.Run()
