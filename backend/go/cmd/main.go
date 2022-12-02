d501d1108def1e56531f10d9367549b4a8015241
Yusei Mizutani <yusei4520030401@gmail.com>
2022-12-02 10:56:34 +0900

    環境変数の追加

--- backend/go/cmd/main.go
+++ backend/go/cmd/main.go
@@ -1,14 +1,14 @@
 package main
 
 import (
-	"fmt"
-	"github.com/karasuneo/aikodai-annai-suru-zo/go/lib"
-	// "github.com/karasuneo/aikodai-annai-suru-zo/go/router"
+	// "fmt"
+	// "github.com/karasuneo/aikodai-annai-suru-zo/go/lib"
+	"github.com/karasuneo/aikodai-annai-suru-zo/go/router"
 )
 
 func main() {
-	trunsctionResult, err := lib.HelloWorld("neo4j://172.21.0.1:57687", "neo4j", "admin")
-	fmt.Println(trunsctionResult, err)
-	// router.Init()
+	// trunsctionResult, err := lib.HelloWorld("neo4j://172.21.0.1:57687", "neo4j", "admin")
+	// fmt.Println(trunsctionResult, err)
+	router.Init()
 	
 }
