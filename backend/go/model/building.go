4daf6c0c40986a83de7efa215f60c10933f4159f
Yusei Mizutani <yusei4520030401@gmail.com>
2022-12-06 11:23:00 +0900

    GETで検索できるようにした

--- backend/go/model/building.go
+++ backend/go/model/building.go
@@ -11,6 +11,12 @@ type Building struct {
 // 建物名を検索してデータを取得
 func GetBuildingName(building_name string) []*Building {
 	result := []*Building{}
+
+	// building_nameが空文字だった時
+	if building_name == "" {
+		return result
+	}
+	
 	db.Where("building_name LIKE ?", "%"+building_name+"%").Find(&result)
 	result = CombineBuilding(result)
 	return result
